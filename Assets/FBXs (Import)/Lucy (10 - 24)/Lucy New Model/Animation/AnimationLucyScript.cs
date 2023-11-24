8a4ac795817b92fda6a8b2a28467c3b590ede897
lalaroann <maenald_008@yahoo.com>
2023-11-24 18:11:28 -0500

    Toon Shader Adjustment + Animation / Movement FIX + Subway FBX texture added

---
+++ Assets/FBXs (Import)/Lucy (10 - 24)/Lucy New Model/Animation/AnimationLucyScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationLucyScript : MonoBehaviour
+{
+    //The Animator name
+    Animator lucyAnimator; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getting the component animator in the object
+        lucyAnimator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")|| Input.GetKey("a")||Input.GetKey("s")||Input.GetKey("d"))
+        {
+            lucyAnimator.SetBool("isRunning", true);
+        }
+        else{
+            lucyAnimator.SetBool("isRunning", false);
+        }
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
     [SerializeField] private Rigidbody rb;
+
+    //(Roann) Rotation speed from turning one direction to another:
+    [SerializeField] public float rotationSpeed;
     void Start()
     {
         
@@ -15,9 +19,33 @@ public class Movement : MonoBehaviour
     {
         if (songManager.instance.gameState == true)
         {
-            var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
+            UnityEngine.Vector3 movement = new UnityEngine.Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            // movement.Normalize();
             rb.velocity = movement * speed;
+        //Roann's Movement Rotation Script
+
+        // transform.Translate(movement);
+
+        if (movement != UnityEngine.Vector3.zero) //Checks if the object/character is not moving - if it IS then run this statement
+        {
+
+            // transform.forward = movement; //Faces the object forward where the the vector 3 is moving INSTANTLY
+
+            //I had to use "UnityEngine." in Quaternion and Vector3 because its referring to UnityEngine and not System.Numerics. 
+            //I gotta specify that Im using UnityEngine namespace and not the other.
+            
+            UnityEngine.Quaternion toRotation =  UnityEngine.Quaternion.LookRotation(movement, UnityEngine.Vector3.up); //This line rotates the object in the desired direction where FORWARD is MOVEMENT and UP is the Y-axis (it never change).
+            transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        }
+
+
     }
 }
+
+
+/*Rotation smoothing reference:
+https://www.youtube.com/watch?v=BJzYGsMcy8Q - Rotating a Character in the Direction of Movement (Unity Tutorial)
+
+*/
